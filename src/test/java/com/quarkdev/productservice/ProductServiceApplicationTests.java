2dd9f6ed090140907b6dda262b450c5b55d19e61
EstebanGC <johan.gal.car@gmail.com>
2024-03-30 18:49:21 -0500

    Setting up Product Service

--- src/test/java/com/quarkdev/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/quarkdev/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,23 @@
 package com.quarkdev.productservice;
 
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
 class ProductServiceApplicationTests {
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
 
-	@Test
-	void contextLoads() {
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
 
 }
