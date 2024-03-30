08ffb0e3cb2edeed42260261e3b3ee13ab3dd90c
EstebanGC <johan.gal.car@gmail.com>
2024-03-29 23:10:33 -0500

    Product model, dto, controller, service, repository setted up

--- src/main/java/com/quarkdev/productservice/controller/ProductController.java
+++ src/main/java/com/quarkdev/productservice/controller/ProductController.java
@@ -1,16 +1,33 @@
 package com.quarkdev.productservice.controller;
 
 import com.quarkdev.productservice.dto.ProductRequest;
+import com.quarkdev.productservice.dto.ProductResponse;
+import com.quarkdev.productservice.model.Product;
+import com.quarkdev.productservice.repository.ProductRepository;
+import com.quarkdev.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
-        return ;
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
